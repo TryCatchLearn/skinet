ba5bef588d4f12907fec00c7b06b9f4dfee2dfdc
Neil Cummings <trycatchlearn@outlook.com>
2025-06-27 12:46:42 +0700

    End of section 7

---
+++ client/src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  imports: [],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
+})
+export class AppComponent {
+  title = 'Skinet';
+}
